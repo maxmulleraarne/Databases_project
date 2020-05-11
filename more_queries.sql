05da874d5ccf9b7c836ef16d7270c5511bc77cfd
Viet Tran <vieteri@DESKTOP-32KQB7B.localdomain>
2020-05-11 21:14:14 +0300

    more select queries added

---
+++ more_queries.sql
@@ -0,0 +1,28 @@
+SELECT *
+FROM Qualification
+WHERE type='Machine operation';
+
+SELECT type
+FROM has_qualification
+WHERE SSID=’445’
+
+SELECT project_ID, Project.description
+FROM Company, Project
+WHERE Company.name='Work bois'
+
+SELECT model
+FROM Machine, Company
+WHERE Machine.company_ID=Company.company_ID AND Company.name='Work bois'
+
+SELECT Employee.name
+FROM Employee, Assignment
+WHERE Employee.SSID=Assignment.SSID AND project.id=666;
+
+SELECT Employee.name
+FROM Employee
+WHERE Employee.SSID NOT IN (SELECT SSID FROM Assignment);
+
+SELECT COUNT(SSID)
+FROM  Employee
+WHERE company_ID=123
+
